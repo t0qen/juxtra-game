ebf29a36935621aa326aefe16e35268379d58095
𝒅𝒓𝒆𝒂𝒎𝒔 <mybeautifuladress@gmail.com>
2025-02-12 21:39:20 +0100

    Better tileset, better code but not finished, other sprite sheet

--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -11,8 +11,8 @@ extends CharacterBody2D
 @onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_fall * jump_time_to_peak)) * -1.0
 @onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_fall * jump_time_to_fall)) * -1.0
 
-@onready var sprites: AnimatedSprite2D = $Sprites # get the animated sprite node from player
- 
+@onready var sprites = $Sprites # get the animated sprite node from player
+
 enum STATE { # each states of player for finite state machine
 	IDLE,
 	RUN,
@@ -28,15 +28,18 @@ var current_speed : float = base_speed # store the current speed
 var want_to_jump : bool = false # a bool to know if player want to jump
 var direction : int = 0 # the direction of player, maybe a Vector2 ? consider jump as direction ?
 var want_to_dash : bool = false # same for dash
+var jumping : bool = false
+var exit_wait : bool = false
 
 func _ready() -> void: # first we set the state of player to idle
-	_set_state(STATE.IDLE)
+	_set_state(STATE.WAIT)
 	set_player_1()
 	
 func _physics_process(delta: float) -> void: # each frame we call this function to update player state, for example if he's not on ground we set his state to fall, etc
 	get_inputs()
 	_update_state(delta)
-	Engine.time_scale = 0.1
+	#print(jumping)
+	Engine.time_scale = 0.01
 	
 func get_inputs(): # essential function to get player inputs, depend on wich player is 
 	if current_player == 1: # if current player is 1 get input with player's 1 inputs
@@ -55,16 +58,22 @@ func _set_state(new_state: STATE) -> void: # simple function to change player's 
 func _enter_state() -> void: # enter transition, basiclly we play animations
 	match current_state:
 		STATE.IDLE:
-			play_animation("idle")
+			play_animation("switch_to_idle")
 		STATE.RUN: 
 			current_speed = base_speed # set on ground speed
 			play_animation("run")
 		STATE.JUMP: 
 			velocity.y = jump_velocity # apply jump
+			jumping = true
 			play_animation("jump")
 		STATE.FLY: 
 			current_speed = air_speed # set air speed
 			play_animation("fly")
+		STATE.WAIT:
+			play_animation("wait")
+			exit_wait = false
+			await get_tree().create_timer(2).timeout
+			exit_wait = true
 
 func _exit_state() -> void: # exit transition, nothing for now
 	match current_state:
@@ -75,7 +84,7 @@ func _exit_state() -> void: # exit transition, nothing for now
 			pass
 			
 		STATE.JUMP: 
-			pass
+			jumping = false
 			
 		STATE.FLY: 
 			pass
@@ -85,52 +94,56 @@ func _update_state(delta: float) -> void:  # every behavior of each states  TO F
 		STATE.IDLE: 
 			if direction: # ef left or right is pressed, start walking
 				_set_state(STATE.RUN)
-			elif !is_on_floor(): # if not on floor, fall down
-				_set_state(STATE.FLY)
+				
 			elif want_to_jump: # jump action, come with inputs system
 				_set_state(STATE.JUMP) # if the jump button is pressed, then jump
-			
+				
+			elif !is_on_floor() && !jumping: # if not on floor, fall down
+				_set_state(STATE.FLY)
+				
 		STATE.RUN:
-			velocity.x = direction * current_speed # Set the move direction
-			flip_sprites()
-				
-			# if not on floor, fall down so fly 
-			if want_to_jump:
+			x_move()
+			if !direction: 
+				_set_state(STATE.WAIT)
+			elif want_to_jump:
 				_set_state(STATE.JUMP) # if jump is pressed, jump
-			else:
-				if !is_on_floor(): 
-					_set_state(STATE.FLY)
-			if velocity.x == 0: # if standing still, then set idle
-				_set_state(STATE.IDLE)
-				
+			elif !is_on_floor() && !jumping: 
+				_set_state(STATE.FLY)
+			
 			move_and_slide() 
 			
 		STATE.JUMP: 
-			velocity.x = direction * current_speed # set the move direction
-			flip_sprites() # flip player's sprite with his direction
-				
-			if !is_on_floor(): # if in the air, apply gravity
-				velocity.y += get_current_gravity() * delta
-			
-			if is_on_floor():
-				print("ON FLOOR")		
+			x_move()
+			if is_on_floor(): 
 				if direction:
 					_set_state(STATE.RUN)
-				else:
-					print("set idle")
-					_set_state(STATE.IDLE)
-					
+				#else:
+					#_set_state(STATE.WAIT)
+			else:	
+				apply_gravity(delta)
 			move_and_slide()
 			
 		STATE.FLY: 
-			velocity.x = direction * current_speed # Set the move direction
-			flip_sprites()
+			x_move()
 			if is_on_floor(): # If the ground is reached, change back to idle
-				_set_state(STATE.IDLE)
-			else: # if still in the air, apply gravity
-				velocity.y += get_current_gravity() * delta
+				if direction:
+					_set_state(STATE.RUN)
+				else:
+					_set_state(STATE.WAIT)
+			else: 
+				apply_gravity(delta)
 			move_and_slide()
 			
+		STATE.WAIT:
+			if exit_wait:
+				_set_state(STATE.IDLE)
+			if direction: # ef left or right is pressed, start walking
+				_set_state(STATE.RUN)
+			if want_to_jump: # jump action, come with inputs system
+				_set_state(STATE.JUMP) # if the jump button is pressed, then jump	
+			if !is_on_floor() && !jumping: # if not on floor, fall down
+				_set_state(STATE.FLY)
+				
 func set_player_1(): # we can choose for player 1 or player 2 when multiplayer, so keyboard will change
 	current_player = 1
 	
@@ -149,6 +162,10 @@ func play_animation(animation: String): # play animation
 				sprites.play("idle_1")
 			"fly":
 				sprites.play("fly_1")
+			"switch_to_idle":
+				sprites.play("switch_to_idle_1")
+			"wait":
+				sprites.play("wait_1")
 	elif current_player == 2: # aniamtions with player's 2 skin
 		match animation: 
 			"jump":
@@ -159,7 +176,11 @@ func play_animation(animation: String): # play animation
 				sprites.play("idle_2")
 			"fly":
 				sprites.play("fly_2")	
-				
+			"switch_to_idle":
+				sprites.play("switch_to_idle_2")
+			"wait":
+				sprites.play("wait_2")
+					
 func get_current_gravity() -> float: # return gravity
 	return jump_gravity if velocity.y < 0.0 else fall_gravity
 
@@ -168,11 +189,13 @@ func flip_sprites():
 		sprites.flip_h = true
 	elif velocity.x < 0:
 		sprites.flip_h = false
-		
-		
-		
-		
-		
+
+func x_move():
+	velocity.x = direction * current_speed 
+	flip_sprites() # flip player's sprite with his direction
+	
+func apply_gravity(delta):
+	velocity.y += get_current_gravity() * delta	
 		
 		
 		
