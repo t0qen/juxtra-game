21dd4638812d88aa232356be06061eb9ec66a968
𝒅𝒓𝒆𝒂𝒎𝒔 <mybeautifuladress@gmail.com>
2025-03-25 18:03:23 +0100

    Improved ball's physic

--- scenes/scripts/ball.gd
+++ scenes/scripts/ball.gd
@@ -2,8 +2,8 @@
 extends RigidBody2D
 
 @export var max_speed: float = 800.0  # Vitesse maximale
-@export var propulsion_force: float = 200.0  # Force de propulsion en l'air
-@export var stop_threshold: float = 50  # Seuil pour considérer que le corps est à l'arrêt
+@export var propulsion_force: float = 20.0  # Force de propulsion en l'air
+@export var stop_threshold: float = 40  # Seuil pour considérer que le corps est à l'arrêt
 var is_on_area_propulsion = false
 
 func _physics_process(delta: float) -> void:
